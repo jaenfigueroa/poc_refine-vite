d4d2d03d6d53265d056e86dedcdb24006d3a8072
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-09-07 11:09:32 -0500

    chore: add storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,12 +2,7 @@
 
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'prettier', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
