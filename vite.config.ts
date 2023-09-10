c65e8047681087691823865e25ad9d98d21c5cb1
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-09-09 23:41:30 -0500

    chore: update basepath

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+const getBase = process.env.BASE_PATH ? { base: process.env.BASE_PATH } : {}
 
 export default defineConfig({
   plugins: [react()],
-});
+  ...getBase,
+})
